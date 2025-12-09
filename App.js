2c3e8c4c10a1e2cb872e9e3efc33960f5c1c0558
Your Name <abhisekgoswami@gmail.com>
2025-12-09 23:25:39 +0530

    E2

--- App.js
+++ App.js
@@ -1,9 +1,40 @@
-    const parent = React.createElement("div",{id:"parent"},
-        React.createElement("div",{id:"child"},
-            React.createElement("h1",{},"I am inside H1 tag")
-        )
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//React Element
+const jsxHeading = (
+    <h1 id="heading" className="head">Namaste React using JSX</h1>
+)
+
+//React Component
+//Class Based Component=>Old way
+//Function Based Component=>New way
+
+//React functional component
+
+// const HeadingComponent = () => {
+//     return (
+//         <h1 id="heading" className="head">Namaste React using Functional Component</h1>
+//     )
+// }
+//Or we can write 
+
+const Title =()=> (
+    <h1 id="heading" className="head">Namaste React using JSX</h1>
+);
+
+
+//Component Composition
+ const HeadingComponent = () => (
+    <div id="container">
+        <Title/>
+        <h1 id="heading" className="head">
+           Namaste React using Functional Component
+        </h1>
+    </div>
+
     )
-    
-    const heading = React.createElement("h1",{id:"heading"},"Hello World from React!!");
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(parent);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
